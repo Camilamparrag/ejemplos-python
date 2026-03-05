5289631e97de0fa05930fc8224d110f907769206
Camilamparrag <camilaparragon@gmail.com>
2026-03-05 01:08:08 +0000

    prueba1

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+print("holaaaaaaaaaaaa")
+
+#VARIABLES
+
+nombre = "Camila"
+edad = 33
+altura = 1.64
+es_profesor = False
+ciudad = "Arica"
+
+
+
+#TIPOS DE DATOS
+
+str #esto es un texto
+int #esto es un entero
+float #esto es un flotante o decimal
+bool #esto es verdadero o falso
+
+#INPUT
+#INPUT()
+
+texto = input()
+print(texto)
