a223415cb14317ef5a3e8b538cee1437a5309fc0
Camilamparrag <camilaparragon@gmail.com>
2026-03-10 00:59:26 +0000

    practicando listas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-print("holaaaaaaaaaaaa")
+"""print("holaaaaaaaaaaaa")
 
 #VARIABLES
 
@@ -24,3 +24,77 @@ bool #esto es verdadero o falso
 
 texto = input()
 print(texto)
+
+
+
+##Listas
+lista = [] ##corchetes son importantes
+##
+diccionario = {"key":"value"}
+##Sets
+sets={}
+##tuplas
+tuplas = ()"""
+
+##Listas
+##index
+frutas = ["manzana", "uva", "kiwi", "sandia", [1,2,3,4,[5,6,7]]]
+    #index    0        1       2       3              4        = posicion
+
+frutas[3] = "pera" #modificar indice
+frutas[0] = "melon" #modificar indice
+frutas[4] = [] #modificar por una lista vacia 
+
+"""print(frutas[2]) #imprimir lista
+print(frutas[1])
+print(frutas[0])
+print(frutas[4][4][0])"""
+
+
+#print(dir(frutas))
+
+#print(dir(diccionario))
+
+#append
+frutas.append("naranaja")
+
+#remove
+frutas.remove("kiwi")
+
+#print(frutas)
+
+personas = ["cristian", "esteban"]
+
+for i in personas: #i lee cada indice
+    print(i)
+
+alumnos = []
+
+while True:
+    nombre = input("ingresa un nombre de un alumno :")
+
+    if nombre == "salir":
+        break
+    alumnos.append(nombre)
+    print(alumnos)
+
+
+
+
+
+
+##diccionarios
+
+
+
+
+
+
+
+
+
+
+
+
+
+
